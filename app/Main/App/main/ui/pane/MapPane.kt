89a8d9ba74038561d36a75633ee73f0d44dfe5a0
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-10-11 12:30:44 +0700

    feat(wold-screen): wip map pane

--- app/Main/App/main/ui/pane/MapPane.kt
+++ app/Main/App/main/ui/pane/MapPane.kt
@@ -25,6 +25,37 @@ class MapPaneState {
     val organism = mutableStateListOf<Organism>()
     val magnification = mutableStateOf(1f)
     val pointerPosition = mutableStateOf<Offset?>(null)
+
+    fun organismRects(drawScope: DrawScope): List<Pair<Organism, Rect?>> {
+        return with(drawScope) {
+            val frameRef = Offset.Zero + center
+
+            val organism = organism
+            val organismSize = Size(10.dp.toPx(), 10.dp.toPx())
+                .div(magnification.value)
+
+            organism.map { organism ->
+                val magnification = magnification.value
+
+                val lat = organism.status?.firstOrNull { status -> status.name == "LAT" }?.value?.toDouble()
+                    ?.times(magnification)
+                    ?.toFloat()
+
+                val lng = organism.status?.firstOrNull { status -> status.name == "LNG" }?.value?.toDouble()
+                    ?.times(magnification)
+                    ?.toFloat()
+
+                // no position provided, cannot draw
+                requireNotNull(lat) { return@map organism to null }
+                requireNotNull(lng) { return@map organism to null }
+
+                val topLeft = frameRef + Offset(lat, lng)
+                val bottomRight = organismSize.toRect().bottomRight
+
+                organism to Rect(topLeft, topLeft + bottomRight)
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -39,49 +70,30 @@ fun MapPane(
             // detect zoom
             .onZoom { state.magnification.value *= it }
     ) {
-        val frameRef = Offset.Zero + center
         val pointerPosition = state.pointerPosition.value
+        val organismRects = state.organismRects(this)
 
-        // draw organism
-        val organism = state.organism
-        val organismSize = Size(10.dp.toPx(), 10.dp.toPx())
-            .div(state.magnification.value)
-        val organismRects = organism.map { organism ->
-            val magnification = state.magnification.value
-
-            val lat = organism.status?.firstOrNull { status -> status.name == "LAT" }?.value?.toDouble()
-                ?.times(magnification)
-                ?.toFloat()
-
-            val lng = organism.status?.firstOrNull { status -> status.name == "LNG" }?.value?.toDouble()
-                ?.times(magnification)
-                ?.toFloat()
-
-            // no position provided, cannot draw
-            requireNotNull(lat) { return@map organism to null }
-            requireNotNull(lng) { return@map organism to null }
-
-            val topLeft = frameRef + Offset(lat, lng)
-            val bottomRight = organismSize.toRect().bottomRight
-
-            organism to Rect(topLeft, topLeft + bottomRight)
-        }
-
+        // layer 1, organism map
         organismRects.forEach { organismRect ->
             val rect = organismRect.second
             requireNotNull(rect) { return@forEach }
 
-            if (pointerPosition != null && pointerPosition in rect) {
-                scale(2f, 2f, rect.center) {
-                    drawRect(
-                        color = Color.Blue,
-                        size = rect.size,
-                        topLeft = rect.topLeft
-                    )
-                }
-            } else {
+            drawRect(
+                color = Color.Black,
+                size = rect.size,
+                topLeft = rect.topLeft
+            )
+        }
+
+        // layer 2, status view
+        requireNotNull(pointerPosition) { return@Canvas }
+        organismRects.filter {
+            it.second != null && pointerPosition in it.second!!
+        }.map {
+            val rect = it.second!!
+            scale(2f, 2f, rect.center) {
                 drawRect(
-                    color = Color.Black,
+                    color = Color.Blue,
                     size = rect.size,
                     topLeft = rect.topLeft
                 )
